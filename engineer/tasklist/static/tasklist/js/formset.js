25fa359b7eb0112f87c8458773dd16b828b03606
ferbthe2gadgetguy <ferbnone@yahoo.com>
2026-08-19 15:59:58 +0800

    Returned True or False logic

--- engineer/tasklist/static/tasklist/js/formset.js
+++ engineer/tasklist/static/tasklist/js/formset.js
@@ -82,7 +82,7 @@ function initFormset() {
     });
 
 
-    tbody.addEventListener("click", (e)=>{
+    tbody.addEventListener("click", (e) => {
 
         if (!e.target.classList.contains("delete-row"))
             return;
@@ -107,5 +107,43 @@ function initFormset() {
 
     });
 
+
+    tbody.addEventListener("change", (e) => {
+
+        const target = e.target;
+
+
+        if (target.matches("input[name$='successful']")) {
+
+            const row =
+                target.closest(".form-row");
+
+            const failed =
+                row.querySelector(
+                    "input[name$='failed']"
+                );
+
+            if (target.checked && failed) {
+                failed.checked = false;
+            }
+        }
+
+
+        if (target.matches("input[name$='failed']")) {
+
+            const row =
+                target.closest(".form-row");
+
+            const successful =
+                row.querySelector(
+                    "input[name$='successful']"
+                );
+
+            if (target.checked && successful) {
+                successful.checked = false;
+            }
+        }
+
+    });
 
 }
